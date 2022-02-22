a783105f7a19ad7ba125c8d2eba7f2af5723c698
naima <naimaosman203@gmail.com>
2022-02-22 13:46:24 +0000

    added coverage file inot .gitignore file

--- Gemfile
+++ Gemfile
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
-gem "date"
+gem 'date'
 
-gem "simplecov"
+gem 'simplecov', require: false, group: :test
+
+gem 'rubocop'
--- lib/account.rb
+++ lib/account.rb
@@ -1,22 +1,22 @@
-class Account
+# frozen_string_literal: true
 
+class Account
   DEFAULT_BALANCE = 0
 
-    def initialize
-      @balance = DEFAULT_BALANCE
-    end
+  def initialize
+    @balance = DEFAULT_BALANCE
+    @tranction = Tranction.new
+  end
 
-    def balance 
-      @balance 
-    end
+  attr_reader :balance
 
-    def deposit(amount)
-      @balance += amount
-    end
+  def deposit(amount)
+    @balance += amount
+  end
 
-    def withdraw(amount)
-      raise "Insufficient funds: Please bring your account into credit" if @balance < amount
-      
-      @balance -= amount
-    end
+  def withdraw(amount)
+    raise 'Insufficient funds: Please bring your account into credit' if @balance < amount
+
+    @balance -= amount
+  end
 end
